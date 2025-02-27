01f7e7b0d36ce8904c321130638fb40be45f7670
Rizki Sadewa <rizkykysadewa@gmail.com>
2025-02-27 22:58:50 +0700

    adding db connection

---
+++ config/database.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable", 
+		os.Getenv("DB_HOST")
+		os.Getenv("DB_USER")
+		os.Getenv("DB_PASSWORD")
+		os.Getenv("DB_NAME")
+		os.Getenv("DB_PORT")
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Could not connect to database : %v", err)
+	}
+
+	// Configure connection pool 
+	sqlDB, _ := db.DB()
+	sqlDB.SetMaxOpenConns(10)
+	sqlDB.SetMaxIdleConns(5)
+	sqlDB.SetConnMaxLifetime(time.hour)
+
+	DB = db
+	log.Println("Database connected successfully...")
+}
